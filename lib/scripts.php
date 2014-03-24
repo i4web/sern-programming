95e6788bcc5d6c9b9e846ad101b93584f0d0fe95
i4web <jrivera@i-4web.com>
2014-03-24 00:12:52 -0400

    Change link color of navbar>brand

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function i4web_scripts() {
-  wp_enqueue_style('i4web_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '2b37fd6990a39f5aed9e2b0a0a944d52');
+  wp_enqueue_style('i4web_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1361a242adb1e1199c22724fcbfb51a7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
