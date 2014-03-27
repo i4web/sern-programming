a74d15c3209be2209a14f89f1842932338cbe8c1
crr <crr@us.ibm.com>
2014-03-27 09:55:04 -0400

    Updated values for skills radial progress bars
    Change-Id: I9dcb76129fe658eb2cb33a2eda97c6e632828b67

--- templates/home-strengths.php
+++ templates/home-strengths.php
@@ -1,19 +1,19 @@
-<? 
+<?
 /**
  * Display the Strengths Area
  */
 ?>
 
 <section class="sern-strengths-area grey-bg" id="sern-scroll">
-  
+
   <h2 class="text-center">my.Strengths();</h2>
- 
+
   <p class="lead text-center">Knight Rider, a shadowy flight into the dangerous world of a man who does not exist. </p>
-  
+
   <div class="container">  <!-- Begin container -->
     <div class="col-sm-4 col-lg-4 sern-strengths-wrapper">  <!-- Begin sern-strengths-wrapper -->
-     
-      <div class="radial-progress radial-a" data-strength="70" data-progress="0">  <!-- Begin Progress Bar -->
+
+      <div class="radial-progress radial-a" data-strength="65" data-progress="0">  <!-- Begin Progress Bar -->
         <div class="circle">
           <div class="mask full">
             <div class="fill"></div>
@@ -27,16 +27,16 @@
 	  <div class="inset">
 		<div class="percentage"></div>
 	  </div>
-	  </div>  <!-- end Progress Bar -->  
-     
-     <div class="sern-strengths-title"> 
+	  </div>  <!-- end Progress Bar -->
+
+     <div class="sern-strengths-title">
        <p>Game Development<p>
-     </div> 
-     
+     </div>
+
     </div> <!-- end sern-strengths-wrapper -->
-    
+
     <div class="col-sm-4 col-lg-4 sern-strengths-wrapper">  <!-- Begin sern-strengths-wrapper -->
-    
+
       <div class="radial-progress radial-b" data-strength="20" data-progress="0">  <!-- Begin Progress Bar -->
         <div class="circle">
           <div class="mask full">
@@ -51,15 +51,15 @@
 	  <div class="inset">
 		<div class="percentage"></div>
 	  </div>
-	  </div>  <!-- end Progress Bar -->  
-     
-     <div class="sern-strengths-title"> 
+	  </div>  <!-- end Progress Bar -->
+
+     <div class="sern-strengths-title">
        <p>Web Development<p>
-     </div>     
-    
+     </div>
+
     </div>  <!-- end sern-strengths-wrapper -->
     <div class="col-sm-4 col-lg-4 sern-strengths-wrapper">  <!-- Begin sern-strengths-wrapper -->
-      <div class="radial-progress radial-c" data-strength="10" data-progress="0">  <!-- Begin Progress Bar -->
+      <div class="radial-progress radial-c" data-strength="90" data-progress="0">  <!-- Begin Progress Bar -->
         <div class="circle">
           <div class="mask full">
             <div class="fill"></div>
@@ -73,12 +73,12 @@
 	  <div class="inset">
 		<div class="percentage"></div>
 	  </div>
-	  </div>  <!-- end Progress Bar -->  
-     
-     <div class="sern-strengths-title"> 
+	  </div>  <!-- end Progress Bar -->
+
+     <div class="sern-strengths-title">
        <p>Software Development<p>
-     </div> 
-    </div>   <!-- end sern-strengths-wrapper -->     
+     </div>
+    </div>   <!-- end sern-strengths-wrapper -->
   </div>  <!-- end container -->
   <strong></strong>
 
